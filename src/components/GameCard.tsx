1c237dc5022b502f5563232b5678c65a436d4e1f
Hiba Hammami <selena.zmorod@gmail.com>
2023-10-18 09:47:18 +0300

    iconList

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, Image, CardHeader } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   games: Game;
@@ -8,10 +8,11 @@ interface Props {
 
 const GameCard = ({ games }: Props) => {
   return (
-    <Card border={"25"} overflow="hidden">
+    <Card borderEndRadius={"10"} overflow="hidden">
       <Image src={games.background_image} />
       <CardBody>
         <CardHeader fontSize="2xl">{games.name}</CardHeader>
+        <PlatformIconList platforms={games.platforms.map((p) => p.platform)} />
       </CardBody>
     </Card>
   );
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,46 @@
+import { Icon, HStack } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    playstation5: FaPlaystation,
+    playstation4: FaPlaystation,
+    xbox: FaXbox,
+    "xbox-series-x": FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <>
+      <HStack marginY={1}>
+        {platforms.map((p) => (
+          <Icon key={p.id} as={iconMap[p.slug]} color={"gray.400"} />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
